707e8396871b1d3d7bd53fc2c09e9950fb103658
Marat Gumerov <marat.gumerof@aspose.com>
2020-07-01 22:55:02 +0400

    Added MapiContact and MapiMessage tests

--- src/test/java/com/aspose/email/cloud/sdk/api/EmailModelTests.java
+++ src/test/java/com/aspose/email/cloud/sdk/api/EmailModelTests.java
@@ -3,20 +3,21 @@ package com.aspose.email.cloud.sdk.api;
 import com.aspose.email.cloud.sdk.invoker.ApiException;
 import com.aspose.email.cloud.sdk.model.EmailDto;
 import com.aspose.email.cloud.sdk.model.MailAddress;
-import com.aspose.email.cloud.sdk.model.MapiContactDto;
 import com.aspose.email.cloud.sdk.model.MapiMessageDto;
-import com.aspose.email.cloud.sdk.model.requests.*;
+import com.aspose.email.cloud.sdk.model.requests.ConvertEmailModelToFileRequestData;
+import com.aspose.email.cloud.sdk.model.requests.ConvertEmailModelToMapiModelRequestData;
+import com.aspose.email.cloud.sdk.model.requests.ConvertEmailRequestData;
+import com.aspose.email.cloud.sdk.model.requests.GetEmailFileAsModelRequestData;
 import org.testng.annotations.Test;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Collections;
 import java.util.Date;
 
 public class EmailModelTests extends TestBase {
     private static final String from = "from@aspose.com";
     private static final EmailDto emailDto = new EmailDto()
         .from(new MailAddress().address(from))
-        .to(Collections.singletonList(new MailAddress().address("to@aspose.com")))
+        .addToItem(new MailAddress().address("to@aspose.com"))
         .subject("Some subject")
         .body("Some body")
         .date(new Date());
---
+++ src/test/java/com/aspose/email/cloud/sdk/api/MapiContactTests.java
@@ -0,0 +1,58 @@
+package com.aspose.email.cloud.sdk.api;
+
+import com.aspose.email.cloud.sdk.model.*;
+import com.aspose.email.cloud.sdk.model.requests.*;
+import org.testng.annotations.Test;
+
+import java.io.UnsupportedEncodingException;
+import java.util.UUID;
+
+public class MapiContactTests extends TestBase {
+    private static final MapiContactDto mapiContact = new MapiContactDto()
+        .electronicAddresses(new MapiContactElectronicAddressPropertySetDto()
+            .defaultEmailAddress(new MapiContactElectronicAddressDto()
+                .emailAddress("email@aspose.com")))
+        .nameInfo(new MapiContactNamePropertySetDto()
+            .givenName("Alex")
+            .surname("Thomas"))
+        .personalInfo(new MapiContactPersonalInfoPropertySetDto()
+            .businessHomePage("www.aspose.com"))
+        .professionalInfo(new MapiContactProfessionalPropertySetDto()
+            .profession("GENERAL DIRECTOR"))
+        .telephones(new MapiContactTelephonePropertySetDto()
+            .primaryTelephoneNumber("+49 211 4247 21"));
+
+    @Test(groups = {"pipeline"})
+    public void ModelToContactDtoTest() {
+        ContactDto contactDto = api.convertMapiContactModelToContactModel(
+            new ConvertMapiContactModelToContactModelRequestData(
+                mapiContact));
+        assert mapiContact.getNameInfo().getGivenName().equals( contactDto.getGivenName());
+        assert mapiContact.getNameInfo().getSurname().equals( contactDto.getSurname());
+    }
+
+    @Test(groups = {"pipeline"})
+    public void ModelToFileTest() throws UnsupportedEncodingException {
+        byte[] vcardBytes = api.convertMapiContactModelToFile(
+            new ConvertMapiContactModelToFileRequestData(
+                "VCard", mapiContact));
+        String vcardString = new String(vcardBytes, "UTF-8");
+        assert vcardString.contains(mapiContact.getNameInfo().getGivenName());
+        MapiContactDto mapiContactConverted = api.getContactFileAsMapiModel(
+            new GetContactFileAsMapiModelRequestData("VCard", vcardBytes));
+        assert mapiContact.getNameInfo().getSurname().equals( mapiContactConverted.getNameInfo().getSurname());
+    }
+
+    @Test(groups = {"pipeline"})
+    public void StorageTest() {
+        String fileName = UUID.randomUUID().toString() + ".msg";
+        api.saveMapiContactModel(
+            new SaveMapiContactModelRequestData(
+                "Msg", fileName, new StorageModelRqOfMapiContactDto(
+                mapiContact, new StorageFolderLocation(storage, folder))));
+        MapiContactDto mapiContactFromStorage = api.getMapiContactModel(
+            new GetMapiContactModelRequestData(
+                "Msg", fileName, folder, storage));
+        assert mapiContact.getNameInfo().getSurname().equals( mapiContactFromStorage.getNameInfo().getSurname());
+    }
+}
